70c88ee1852c3b244a38b52cd281242a36475b58
彭振辉 <1903301895@qq.com>
2024-09-10 16:06:53 +0800

    03-类的实例化

---
+++ ClassObject/class.cpp
@@ -0,0 +1,48 @@
+//
+// Created by dell on 2024-09-10.
+//
+/**
+ *   面向对象三大特性：封装 继承 多态
+ * ? 具有相同性质的对象，我们可以抽象称为类
+*/
+/*1.封装*/
+//* 将属性和行为作为一个整体，表现生活中的事物
+//* 将属性和行为加以权限控制
+#include<iostream>
+using namespace std;
+// 设计一个圆类，求圆的周长
+// 圆求周长的公式 2 * PI * R
+#define PI 3.14159
+class Circle{
+    //访问权限
+public:
+    //属性
+    int m_r;
+    //行为
+    double calculate(){
+        return 2 * PI * m_r;
+    }
+};
+
+//设计一个学生类，属性有姓名和学号，可以给姓名和学号赋值，可以显示学生的姓名和学号
+class Student{
+public:
+    string stuName;
+    string stuId;
+    string printStuInfo(){
+        return "姓名："+stuName+" 学号："+stuId;
+    }
+};
+int main(){
+    /*通过圆类 创建具体的圆（对象）*/
+//    实例化
+    Circle c1;
+    c1.m_r = 2;
+
+    cout<< "圆的周长为"<<c1.calculate()<<endl;
+
+    Student s1;
+    s1.stuId = "20200220230320";
+    s1.stuName = "照旧";
+    cout<<""<<s1.printStuInfo()<<endl;
+}
